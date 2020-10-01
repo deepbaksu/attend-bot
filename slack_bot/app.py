7382063ad56f9bee1a61ba1e5fef1f005d736ab0
Mo Kweon <kkweon@gmail.com>
2020-10-01 18:32:42 +0000

    Clean up Python code
    - Use Black to format the code
    - Refactor template for readability

--- slack_bot/app.py
+++ slack_bot/app.py
@@ -3,25 +3,42 @@ from flask import request
 import datetime
 from pytz import timezone, utc
 
-KST = timezone('Asia/Seoul')
+KST = timezone("Asia/Seoul")
 app = Flask(__name__)
 
-@app.route('/attend', methods=['GET', 'POST'])
+
+@app.route("/attend", methods=["GET", "POST"])
 def attend():
     now = datetime.datetime.utcnow()
     kr_time = utc.localize(now).astimezone(KST)
 
-    # 윈도우에서는 strftime을 이용하여 날짜형태를 변경하고 한글을 결합할 경우 
+    # 윈도우에서는 strftime을 이용하여 날짜형태를 변경하고 한글을 결합할 경우
     # UnicodeEncodeError 및 Invalid format string 오류가 발생할 수 있습니다.
     # Korean + time.strftime() 문제해결을 위해 다음의 링크를 참고하였습니다.
     # https://hcid-courses.github.io/TA/QnA/issues_with_windows_korean_strftime.html
+    datetime_msg = (
+        kr_time.strftime(
+            "%m월 %d일 출근시간은 한국시각기준 %H시 %M분입니다.".encode("unicode-escape").decode()
+        )
+        .encode()
+        .decode("unicode-escape")
+    )
+
+    username = request.form["user_name"]
+
+    text = f"""*{username}님 출석체크*
+{datetime_msg}
+
+존버는 승리합니다.
+"""
+
     msg = {
-            "response_type": "in_channel",
-            "text":'*'+request.form['user_name'] + '님 출석체크* \n' + \
-            kr_time.strftime('%m월 %d일 출근시간은 한국시각기준 %H시 %M분입니다.\n'.encode('unicode-escape').decode()).encode().decode('unicode-escape') + \
-            '존버는 승리합니다\n\n'} 
+        "response_type": "in_channel",
+        "text": text,
+    }
 
     return jsonify(msg)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
