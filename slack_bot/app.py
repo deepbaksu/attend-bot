25b2e406a02a87a701c14b8ccce7ae010e9fe72b
Jinyoung <40735154+diligejy@users.noreply.github.com>
2020-10-01 17:49:23 +0900

    Update app.py

--- slack_bot/app.py
+++ slack_bot/app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request, Response
 import datetime
 from pytz import timezone, utc
@@ -15,18 +15,15 @@ app = Flask(__name__)
 
 @app.route('/attend', methods=['GET', 'POST'])
 def attend():
-    msg = \
-        '*'+request.form['user_name'] + '님 출석체크* \n' + str(kr_time).split('.')[0].split(' ')[0].split('-')[1] + '월 '+ \
+    msg = {
+        'response_type' : 'in_channel',
+        'text' : '*'+request.form['user_name'] + '님 출석체크* \n' + str(kr_time).split('.')[0].split(' ')[0].split('-')[1] + '월 '+ \
         str(kr_time).split('.')[0].split(' ')[0].split('-')[2] + '일 '+ \
         '출근시간은 한국시각기준 ' + \
         str(kr_time).split('.')[0].split(' ')[1].split(':')[0] + '시 ' + \
         str(kr_time).split('.')[0].split(' ')[1].split(':')[1] + '분입니다.\n' + '존버는 승리합니다.\n\n'
-
-    client.chat_postMessage(channel='#attend', text=msg, headers= {'User-Agent' : 'Chrome/66.0.3359.181'})
-
-    
-    resp = Response(response=None, status=200, mimetype="application/json", headers= {'User-Agent' : 'Chrome/66.0.3359.181'})
-    return resp
+    }
+    return jsonify(msg)
 
 if __name__ == "__main__":
     app.run(debug=True)
