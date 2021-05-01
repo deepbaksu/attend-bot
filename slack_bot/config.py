f62dfe19166fe1ec0f1b6fde7e5e9e7bf85cd7ae
Mo Kweon <kkweon@gmail.com>
2021-05-01 08:06:05 +0000

    DATABASE_URL should return string

--- slack_bot/config.py
+++ slack_bot/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace(
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "").replace(
         "postgres://", "postgresql://"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
