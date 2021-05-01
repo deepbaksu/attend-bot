1f43cda92e1b0af784cf3cb01e5e34fc6a3f0bc8
Mo Kweon <kkweon@gmail.com>
2021-05-01 08:02:41 +0000

    Replace postgres::// with postgresql:://
    The former has been deprecated.

--- slack_bot/config.py
+++ slack_bot/config.py
@@ -2,7 +2,9 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace(
+        "postgres://", "postgresql://"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
