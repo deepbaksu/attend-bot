692b23e251446eb507c1b164eafd10b0d7e09b76
Mo Kweon <kkweon@gmail.com>
2021-10-23 08:47:34 -0700

    fix: remove duplicate quote

--- slack_bot/quote.py
+++ slack_bot/quote.py
@@ -11,18 +11,14 @@ class Quote(yaml.YAMLObject):
     quote: str
     author: str
     title: str
-    skip: bool
 
-    def __init__(
-        self, quote: str, author: str = "", title: str = "", skip: bool = False
-    ):
+    def __init__(self, quote: str, author: str = "", title: str = ""):
         self.quote = quote.strip()
         self.author = author.strip()
         self.title = title.strip()
-        self.skip = skip
 
     def __hash__(self) -> int:
-        return hash((self.quote, self.author, self.title, self.skip))
+        return hash((self.quote, self.author, self.title))
 
     @staticmethod
     def from_dict(d: dict) -> "Quote":
@@ -30,13 +26,10 @@ class Quote(yaml.YAMLObject):
             quote=d.get("quote", "").strip(),
             author=d.get("author", "").strip(),
             title=d.get("title", "").strip(),
-            skip=d.get("skip", False),
         )
 
     def to_dict(self) -> dict:
-        return dict(
-            quote=self.quote, author=self.author, title=self.title, skip=self.skip
-        )
+        return dict(quote=self.quote, author=self.author, title=self.title)
 
     def to_message(self) -> str:
         return f"""{self.quote}
@@ -56,7 +49,5 @@ def load_quotes() -> List[Quote]:
     data = []
     with open("slack_bot/saying.yaml", "r") as f:
         for q_dict in yaml.safe_load(f):
-            quote = Quote.from_dict(q_dict)
-            if not quote.skip:
-                data.append(quote)
+            data.append(Quote.from_dict(q_dict))
     return data
